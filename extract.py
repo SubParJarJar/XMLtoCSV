2d00bdbc6b4d784bff61e4d095c572cf55021c09
Reeds2K <77898328+Reeds2K@users.noreply.github.com>
2021-04-26 22:00:03 +0200

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,11 +1,3 @@
-# 1. Parent searches for data in child (child.text)
-#   a. If data, save to list
-# 2. At the same time checks for grandchildren
-#   a. If grandchildren, add to dict {child_elem: [grandchildren]}.
-#   b. Tag: no_grandchildren is set to False.
-# 3. If no grandchildren, write to file
-# 4. If grandchildren, restard function for child
-
 from lxml import etree
 import InitLogging
 import logging
