22ff2cd0e9c68380e15da94675e3e5b75c68c3c3
Mark Monster <mark.monster@allyourbi.nl>
2021-04-26 17:52:47 +0200

    Works

--- extract.py
+++ extract.py
@@ -1,15 +1,12 @@
-# : Children are passed into function by master.
-#   : Unless master is root, then .getchildren is run twice.
-# : While looping over children, get data of all children.
-#   : This data is saved into a list together with the list of grandchildren.
-#   : If no grandchildren are found, that part of the recursive return 0.
-#   : A tag is added called no_grandchildren which remains True until a grandchild is found.
-#   : If master is at last child in list and no_grandchildren is True:
-#       : Then the last branch is found and another tag is given, write_out.
-#       : If write_out is true then all data is gathered and written to file, and the function exits.
-
-# : Get XML, parse XML, get root, get list of children.
-# : Create recursive function
+# 1. Parent, zoekt naar data in child (child.text).
+#   a. Als data, sla op in lijst.
+# 2. Zoekt tegelijk naar grandchildren.
+#   a. Als grandchildren, voeg toe aan dict {child_elem: [grandchildren]}.
+#   b. Tag: no_grandchildren set to False.
+# 3. Als geen grandchildren.
+#   a. Write to file.
+# 4. Als grandchildren.
+#   a. Start functie voor child met grandchildren.
 
 from lxml import etree
 import InitLogging
@@ -18,11 +15,7 @@ import logging
 InitLogging.init_logging(log_level=0)
 
 
-def add_to_header(d, dictionary, cnt):
-    dictionary[d] = cnt
-
-
-def unpack_xml(element, children_elements, children_data, no_grandchildren=True, counter=0):
+def unpack_xml(element, children_elements, children_data, no_grandchildren=True):
     # Child: [grandchildren]
     children_parameters = dict()
     # pass to child if grandchildren: grandchildren, children_data,
@@ -34,25 +27,23 @@ def unpack_xml(element, children_elements, children_data, no_grandchildren=True,
         logging.debug(f"Data found: {child_data}")
         # If child has text, add to data list
         if child_data[1]:
-            counter += 1
-            add_to_header(child.tag, header_dict, counter)
             gathered_data.append(child_data)
             logging.debug(f"Data added to data list")
 
         grandchildren = child.getchildren()
         logging.debug(f"Grandchildren returned: {grandchildren}")
-        # If child has grandchildren: add child and list of children to dict. These are to be passed in when calling
-        # this function again for the child.
+        # If child has grandchildren: add child and list of children to dict.
+        # These are to be passed in when calling this function again for the child.
         if grandchildren:
             no_grandchildren = False
-            logging.debug(f"Grandchildren found, no_grandschildren set to: {no_grandchildren}")
+            logging.debug(f"Grandchildren found, no_grandchildren set to: {no_grandchildren}")
             children_parameters[child] = grandchildren
             logging.debug(f"Grandchildren have been added to dictionary: {children_parameters}")
     logging.info(f"Information gathering for {element} has completed")
 
     logging.info(f"Checking if grandchildren were found.")
     if no_grandchildren:
-        logging.info(f"No grandchildren were found, starting writeout.")
+        logging.info(f"No grandchildren were found, starting write-out.")
         with open('out.csv', 'a') as f:
             logging.info(f"{element} writing to file")
             for elem in gathered_data:
@@ -75,7 +66,7 @@ def unpack_xml(element, children_elements, children_data, no_grandchildren=True,
         logging.debug(f"Element: {child}")
         logging.debug(f"Children elements list: {grandchildren}")
         logging.debug(f"Data: {gathered_data}")
-        unpack_xml(element=child, children_elements=grandchildren, children_data=gathered_data, counter=counter)
+        unpack_xml(element=child, children_elements=grandchildren, children_data=gathered_data)
         # As long as children_elements has children, pop. If at last one and still no grandchildren, then last in branch
         # found, start writeout.
 
@@ -88,7 +79,6 @@ root_children = root.getchildren()
 data = []
 header_dict = dict()
 unpack_xml(element=root, children_elements=root_children, children_data=data)
-
 
 # TODO: Create dictionary with number:column_name. If key == dictionary[number] then keep writing.
 #   TODO: If not, create new file.
